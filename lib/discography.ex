4a2a14af3d614af6c1410eebc37125d341b6b30b
Matias Gutierrez <gutierrez.matias@outlook.com>
2021-04-22 18:14:43 -0300

    first commit

---
+++ lib/discography.ex
@@ -0,0 +1,18 @@
+defmodule Discography do
+  @moduledoc """
+  Documentation for `Discography`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Discography.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
