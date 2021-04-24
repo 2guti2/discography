2944e339275d8b1db8e350e8805c6a4bc0afc049
Matias Gutierrez <gutierrez.matias@outlook.com>
2021-04-24 16:00:48 -0300

    fix warnings

--- mix.exs
+++ mix.exs
@@ -31,7 +31,7 @@ defmodule Discography.MixProject do
       {:credo, "~> 1.4", only: [:dev, :test], runtime: false},
       {:excoveralls, "~> 0.12", only: :test},
       {:ecto_sql, "~> 3.4"},
-      {:ex_doc, "~> 0.24", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.24", only: :dev, runtime: false}
     ]
   end
 end
