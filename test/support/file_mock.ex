d9492bf94e7ffe14a26f2af0099a745652144874
Matias Gutierrez <gutierrez.matias@outlook.com>
2021-04-23 23:43:41 -0300

    add alias

--- test/support/file_mock.ex
+++ test/support/file_mock.ex
@@ -3,7 +3,9 @@ defmodule Discography.Helpers.FileMock do
   Documentation for `FileMock`.
   """
 
+  alias Discography.Helpers.Streams
+
   def stream!("discography.txt") do
-    Discography.Helpers.Streams.build_stream("1974 Planet Waves\n1975 Blood on the Tracks")
+    Streams.build_stream("1974 Planet Waves\n1975 Blood on the Tracks")
   end
 end
