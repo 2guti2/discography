9fe70fd02297088b5cf51e35ef34651ba7a2ec45
Matias Gutierrez <gutierrez.matias@outlook.com>
2021-04-27 17:33:19 -0300

    fix spec

--- lib/integrations/trello.ex
+++ lib/integrations/trello.ex
@@ -10,7 +10,7 @@ defmodule Discography.Integrations.Trello do
   @type album_list :: [%Album{year: integer(), name: String.t()}]
   @type decade_list :: [%Decade{title: String.t(), albums: album_list()}]
 
-  @spec overwrite_lists(decade_list(), String.t()) :: any()
+  @spec overwrite_lists(decade_list(), String.t()) :: decade_list()
   def overwrite_lists(decades, board_url) do
     if Mix.env() == :dev, do: IO.puts("archiving preexisting lists on board")
 
@@ -25,12 +25,7 @@ defmodule Discography.Integrations.Trello do
     end
   end
 
-  defp archive_preexisting_lists(board_id) do
-    board_id
-    |> API.get_lists()
-    |> API.archive_lists()
-  end
-
+  @spec add_cards_to_lists(decade_list()) :: tuple()
   def add_cards_to_lists(decades) do
     if Mix.env() == :dev, do: IO.puts("adding albums to lists")
 
@@ -43,5 +38,11 @@ defmodule Discography.Integrations.Trello do
     end
 
     {:ok}
+  end
+
+  defp archive_preexisting_lists(board_id) do
+    board_id
+    |> API.get_lists()
+    |> API.archive_lists()
   end
 end
---
+++ test/trello_test.exs
@@ -0,0 +1,5 @@
+defmodule Discography.Test.TrelloTest do
+  use ExUnit.Case
+
+
+end
